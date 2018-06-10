8a42c6e1ec53925f1d685b52d762c6d41ea49d19
Mathew Robinson <chasinglogic@gmail.com>
2018-06-10 19:36:09 -0400

    Write a ruby gem install check

---
+++ redalert/checks/gem_installed.py
@@ -0,0 +1,28 @@
+import subprocess
+
+from .exc import CheckFailure
+
+
+def get_installed_gems():
+    """Return a list of installed gems"""
+    proc = subprocess.run(['gem', 'list'])
+    return str(proc.stdout).split('\n')
+
+
+class GemInstalledCheck:
+    """Checks if gems are installed and optionally checks version"""
+
+    def __init__(self, name, version=""):
+        self.name = name
+        self.version = version
+
+    def check(self):
+        installed_gems = get_installed_gems()
+        for gem in installed_gems:
+            if self.name in gem:
+                if self.version not in gem:
+                    raise CheckFailure(
+                        'gem {} installed but is version {} expected {}'\
+                        .format(self.name, gem, self.version))
+                return
+        raise CheckFailure('{}')
