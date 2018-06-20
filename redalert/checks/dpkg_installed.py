9284dc8843ef042810cc3bc91fcdbcb5b44b5014
Mathew Robinson <chasinglogic@gmail.com>
2018-06-20 17:58:15 -0400

    Using new vagrant tests make dpkg_installed work

--- redalert/checks/dpkg_installed.py
+++ redalert/checks/dpkg_installed.py
@@ -1,6 +1,6 @@
 """dpkg package checkers"""
 
-import apt #pylint: disable-msg=import-error
+import subprocess
 
 from .exc import CheckFailure
 
@@ -14,10 +14,25 @@ class DpkgCheck:
 
     def check(self):
         """Check that the package is installed and optionally the correct version."""
-        cache = apt.Cache()
-        pkg = cache[self.name]
-        if not pkg.is_installed:
+        proc = subprocess.run(
+            ['dpkg', '-s', self.name], stdout=subprocess.PIPE)
+        if proc.returncode != 0:
             raise CheckFailure('package {} is not installed'.format(self.name))
-        if self.version is not None and pkg.installed.version != self.version:
+
+        # End early if version check not necessary
+        if self.version is None:
+            return
+
+        output = str(proc.stdout.decode('utf-8'))
+        version = ''
+        for line in output.splitlines():
+            if line.startswith('Version:'):
+                split = line.split(':')
+                version = split[len(split) - 1]
+                break
+
+        # remove leading and trailing spaces
+        version = version.strip()
+        if version != self.version:
             raise CheckFailure('package {} version {} is installed not {}'\
-                            .format(self.name, pkg.installed.version, self.version))
+                            .format(self.name, version, self.version))
