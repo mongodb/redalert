2411b22393f67c298b83e4d15996486ba80aca52
Mathew Robinson <chasinglogic@gmail.com>
2018-07-16 08:59:54 -0400

    Write doc comment matching design specification

--- checks/file.go
+++ checks/file.go
@@ -6,6 +6,27 @@ import (
 )
 
 // FileChecker checks if a file exists or does not
+//
+// Type:
+//	 - file-exists
+//   - file-does-not-exist
+//
+// Support Platforms:
+//   - Mac
+//   - Linux
+//   - Windows
+//
+// Arguments:
+//   name (required): A string value that points to a path on the filesystem.
+//   exists: An optional boolean indicating whether the file should exist or not.
+//			 For file-does-not-exist type tests this is always set to false, for
+//			 the normal file-exists type tests this value defaults to true, the
+//			 file should exist, but can be set to false if desired.
+//
+// Notes:
+//   For Unix systems no `~` expansion is done. So ~/.bashrc is not a valid path,
+//   or at least will not do what you think it will. Additionally, when checking
+//   paths on Windows provide windows style paths (i.e. C:\My\File\Path.txt).
 type FileChecker struct {
 	Exists bool
 	Name   string
