4e31c3df118ecac7d76a3953af25994e747abc0f
Mathew Robinson <chasinglogic@gmail.com>
2018-07-17 21:52:32 -0400

    Correctly verify that there was an error when ShouldError is true

--- checks/checks_test.go
+++ checks/checks_test.go
@@ -35,6 +35,8 @@ func runCheckerTests(t *testing.T, tests checkerTests, argable ArgableFunc) {
 			t.Logf("%s: Error running checker: %s", test.Name, err)
 		} else if err != nil {
 			t.Errorf("%s: Check error: %s", test.Name, err)
+		} else if err == nil && test.ShouldError {
+			t.Errorf("%s: Didn't get an error and should have.", test.Name)
 		}
 	}
 }
