afd49e4a0edc210945c0caae69860d501a09b7e5
Zakhar Kleyman <zakhar.kleyman@mongodb.com>
2018-07-18 12:02:51 -0400

    add archive to .goreleaser.yml

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/chasinglogic/redalert/commands"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+	fmt.Printf("%v, commit %v, built at %v\n", version, commit, date)
 	commands.Root.Execute()
 }
