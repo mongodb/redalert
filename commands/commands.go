c7e06bf9df5032abf914cf79587fa7bb2113ab08
Tural Farhadov <tural.ferhadov@gmail.com>
2021-04-12 14:49:44 -0700

    Add empty "redalert document" command

--- commands/commands.go
+++ commands/commands.go
@@ -1,13 +1,13 @@
 // Copyright 2018 MongoDB Inc. All rights reserved. Use of this source code is
 // governed by the Apache-2.0 license that can be found in the LICENSE file.
 
-
 package commands
 
 import "github.com/spf13/cobra"
 
 func init() {
 	Root.AddCommand(Run)
+	Root.AddCommand(Document)
 }
 
 // Root CLI command. This should have no functionality.
--- commands/run.go
+++ commands/run.go
@@ -1,7 +1,6 @@
 // Copyright 2018 MongoDB Inc. All rights reserved. Use of this source code is
 // governed by the Apache-2.0 license that can be found in the LICENSE file.
 
-
 package commands
 
 import (
@@ -132,6 +131,14 @@ var Run = &cobra.Command{
 		}
 
 		os.Exit(exitCode)
+	},
+}
+
+var Document = &cobra.Command{
+	Use:   "document",
+	Short: "Document the current image",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello from the ducment command")
 	},
 }
 
