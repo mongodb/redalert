fb950c11560650ce0bc6557adff5d751fd71e4c9
Zakhar Kleyman <zakhar.kleyman@mongodb.com>
2021-04-13 18:04:48 -0400

    feat: get toolchain revisions from preset configs

--- commands/document.go
+++ commands/document.go
@@ -4,12 +4,17 @@
 package commands
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/mongodb/redalert/reports"
 	"github.com/spf13/cobra"
 )
 
+<<<<<<< HEAD
 var (
 	systemtype string
 )
@@ -19,10 +24,45 @@ var (
 // It takes the following flags:
 //
 //  - `--type` tye type of the system: supported values are macos, debian
+=======
+type details struct {
+	Type     string
+	Versions []versions
+}
+
+type versions struct {
+	Name    string
+	Version string
+}
+
+func ReadRevision(fn string) (string, error) {
+	f, err := os.Open(fn)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	bf := bufio.NewReader(f)
+	var line string
+
+	// We expect the Revision to be on the first line
+	line, err = bf.ReadString('\n')
+	line = strings.TrimSuffix(line, "\n")
+	if err != nil {
+		return "", err
+	}
+
+	if line == "" {
+		return "", fmt.Errorf("empty")
+	}
+	return line, nil
+}
+
+>>>>>>> 61f90a4... feat: get toolchain revisions from preset configs
 var Document = &cobra.Command{
 	Use:   "document",
 	Short: "Document the current image",
 	Run: func(cmd *cobra.Command, args []string) {
+<<<<<<< HEAD
 <<<<<<< HEAD
 		if len(systemtype) > 0 {
 			commandRes, err := reports.Packages(systemtype)
@@ -39,6 +79,34 @@ var Document = &cobra.Command{
 			fmt.Printf("Args: %v\n", args)
 		}
 >>>>>>> 84d7906... feat: test args to document command
+=======
+		fmt.Println("Hello from the document command")
+		if len(args) > 0 {
+			fmt.Printf("Args: %v\n", args)
+		}
+		toolchains := make(map[string]string)
+
+		toolchains["mongodb"] = "/tmp/revision.txt"
+		toolchains["python"] = "/tmp/rev_python.txt"
+
+		details := make(map[string]map[string]string)
+		toolchainDetails := make(map[string]string)
+		for name, path := range toolchains {
+			revision, err := ReadRevision(path)
+			if err != nil {
+				fmt.Println(err)
+				toolchainDetails[name] = "none"
+			} else {
+				toolchainDetails[name] = revision
+			}
+			//fmt.Println(toolchainDetails)
+		}
+		details["toolchains"] = toolchainDetails
+		//fmt.Println(details)
+
+		jsonString, _ := json.Marshal(details)
+		fmt.Println(string(jsonString))
+>>>>>>> 61f90a4... feat: get toolchain revisions from preset configs
 	},
 }
 
