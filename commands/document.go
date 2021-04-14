c5185582e353adfb7bc75e98d40c55ec356aaaaf
Tural Farhadov <tural.ferhadov@gmail.com>
2021-04-14 09:44:13 -0700

    Remove pacakge related logic from document.go

--- commands/document.go
+++ commands/document.go
@@ -6,16 +6,10 @@ package commands
 import (
 	"encoding/json"
 	"fmt"
-	"os/exec"
 
 	"github.com/mongodb/redalert/reports"
 	"github.com/spf13/cobra"
 )
-
-type Package struct {
-	Name    string `json:"name"`
-	Version string `json:"version"`
-}
 
 var (
 	systemtype string
@@ -30,35 +24,24 @@ var Document = &cobra.Command{
 	Use:   "document",
 	Short: "Document the current image",
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(systemtype) > 0 {
-			commandRes, err := reports.GetPackagesDetails(systemtype)
-			if err != nil {
-				fmt.Println("ERR: " + err.Error())
-			}
-			fmt.Println(string(commandRes))
-		}
-
-		details := make(map[string]map[string]string)
-		externalCommand := externalCommands[systemtype]
-		command := exec.Command(externalCommand[0], externalCommand[1:]...)
-		commandRes, err := command.CombinedOutput()
-
-		toolchainDetails := reports.GetToolchainDetails()
-		details["toolchains"] = toolchainDetails
-		//fmt.Println(details)
-
-		jsonString, _ := json.Marshal(details)
-		fmt.Println(string(jsonString))
-		commandsParsed := parseCommandOuput(string(commandRes), systemtype)
-
-		formattedPackages, err := formatPacakges(commandsParsed, "json")
-
-		if err != nil {
-			fmt.Println("Could not format the pacakges")
+		if !reports.IsValidSystemType(systemtype) {
+			fmt.Errorf("System type not supported: " + systemtype)
 			return
 		}
 
-		fmt.Println(formattedPackages)
+		details := make(map[string]interface{})
+
+		pacakgeDetails, err := reports.GetPackagesDetails(systemtype)
+		if err != nil {
+			fmt.Println("ERR: " + err.Error())
+		}
+
+		toolchainDetails := reports.GetToolchainDetails()
+		details["toolchains"] = toolchainDetails
+		details["packages"] = pacakgeDetails
+
+		jsonString, _ := json.Marshal(details)
+		fmt.Println(string(jsonString))
 	},
 }
 
